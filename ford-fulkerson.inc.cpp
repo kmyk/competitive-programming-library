a4dde9f20cbb9b30715e39d68e229035a7e8ef21
Kimiyuki Onaka <kimiyuki95@gmail.com>
2016-06-30 23:36:30 +0900

    commit

---
+++ ford-fulkerson.inc.cpp
@@ -0,0 +1,62 @@
+struct edge_t { int to, cap, rev; };
+int maximum_flow_destructive(int s, int t, vector<vector<edge_t> > & g) {
+    int n = g.size();
+    vector<bool> used(n);
+    function<int (int, int)> dfs = [&](int i, int f) {
+        if (i == t) return f;
+        used[i] = true;
+        for (edge_t & e : g[i]) {
+            if (used[e.to] or e.cap <= 0) continue;
+            int nf = dfs(e.to, min(f, e.cap));
+            if (nf > 0) {
+                e.cap -= nf;
+                g[e.to][e.rev].cap += nf;
+                return nf;
+            }
+        }
+        return 0;
+    };
+    int result = 0;
+    while (true) {
+        used.clear(); used.resize(n);
+        int f = dfs(s, numeric_limits<int>::max());
+        if (f == 0) break;
+        result += f;
+    }
+    return result;
+}
+
+int maximum_flow(int s, int t, vector<vector<edge_t> > g /* adjacency list */) { // ford fulkerson, O(FE)
+    return maximum_flow_destructive(s, t, g);
+}
+
+vector<pair<int,int> > perfect_bipartite_matching(set<int> const & a, set<int> const & b, vector<vector<int> > const & g /* adjacency list */) { // O(V + FE)
+    assert (a.size() + b.size() <= g.size());
+    int n = g.size();
+    int src = n;
+    int dst = n + 1;
+    vector<vector<edge_t> > h(n + 2);
+    auto add_edge = [&](int from, int to, int cap) {
+        h[from].push_back((edge_t) {   to, cap, int(h[  to].size()    ) });
+        h[  to].push_back((edge_t) { from,   0, int(h[from].size() - 1) });
+    };
+    repeat (i,n) {
+        if (a.count(i)) {
+            add_edge(src, i, 1);
+            for (int j : g[i]) if (b.count(j)) {
+                add_edge(i, j, 1); // collect edges e : a -> b, from g
+            }
+        }
+        if (b.count(i)) {
+            add_edge(i, dst, 1);
+        }
+    }
+    maximum_flow_destructive(src, dst, h);
+    vector<pair<int,int> > ans;
+    for (int from : a) {
+        for (edge_t e : h[from]) if (b.count(e.to) and e.cap == 0) {
+            ans.emplace_back(from, e.to);
+        }
+    }
+    return ans;
+}
--- union-find-tree.inc.cpp
+++ union-find-tree.inc.cpp
@@ -1,17 +1,18 @@
 // http://abc040.contest.atcoder.jp/submissions/774959
-struct union_find {
-    vector<int> tree;
-    explicit union_find(size_t n) : tree(n, -1) {}
-    bool is_root(int a) { return tree[a] < 0; }
-    int find_root(int a) { return is_root(a) ? a : (tree[a] = find_root(tree[a])); }
-    int tree_size(int a) { return - tree[find_root(a)]; }
-    void union_tree(int a, int b) {
+// http://codeforces.com/contest/687/submission/18818699
+struct disjoint_sets {
+    vector<int> data;
+    explicit disjoint_sets(size_t n) : data(n, -1) {}
+    bool is_root(int a) { return data[a] < 0; }
+    int find_root(int a) { return is_root(a) ? a : (data[a] = find_root(data[a])); }
+    int set_size(int a) { return - data[find_root(a)]; }
+    void union_sets(int a, int b) {
         a = find_root(a); b = find_root(b);
         if (a != b) {
-            if (not (tree_size(a) <= tree_size(b))) swap(a,b);
-            tree[b] += tree[a];
-            tree[a] = b;
+            if (set_size(a) > set_size(b)) swap(a,b);
+            data[b] += data[a];
+            data[a] = b;
         }
     }
-    bool is_connected(int a, int b) { return find_root(a) == find_root(b); }
+    bool is_same(int a, int b) { return find_root(a) == find_root(b); }
 };
