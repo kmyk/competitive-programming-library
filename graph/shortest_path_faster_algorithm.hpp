a918518fde2dda56b9a1bb581c2c9afbd239280d
Kimiyuki Onaka <kimiyuki95@gmail.com>
2019-08-12 20:37:43 +0900

    update SPFA with SLF

--- graph/shortest_path_faster_algorithm.hpp
+++ graph/shortest_path_faster_algorithm.hpp
@@ -2,7 +2,7 @@
 #include <cassert>
 #include <climits>
 #include <cstdint>
-#include <queue>
+#include <deque>
 #include <tuple>
 #include <utility>
 #include <vector>
@@ -16,31 +16,35 @@ std::vector<int64_t> shortest_path_faster_algorithm(int root, std::vector<std::v
     assert (not g.empty());
     int n = g.size();
     std::vector<int64_t> dist(n, INT64_MAX);
-
-    std::queue<int> que;
+    std::deque<int> cur, nxt;
     std::vector<bool> pushed(n);
     dist[root] = 0;
-    que.push(root);
+    nxt.push_back(root);
     pushed[root] = true;
 
     REP (iteration, 2 * n - 2) {
-        que.push(-1);
-        while (not que.empty()) {
-            int x = que.front();
-            que.pop();
-            if (x == -1) break;
+        cur.swap(nxt);
+        while (not cur.empty()) {
+            int x = cur.front();
+            cur.pop_front();
             pushed[x] = false;
             for (const auto & edge : g[x]) {
                 int y; int64_t cost; std::tie(y, cost) = edge;
                 if ((dist[x] == INT64_MIN ? INT64_MIN : dist[x] + cost) < dist[y]) {
                     dist[y] = (iteration >= n - 1 ? INT64_MIN : dist[x] + cost);
                     if (not pushed[y]) {
-                        que.push(y);
+                        if (not nxt.empty() and dist[y] < dist[nxt.front()]) {
+                            // Small Label First
+                            nxt.push_front(y);
+                        } else {
+                            nxt.push_back(y);
+                        }
                         pushed[y] = true;
                     }
                 }
             }
         }
+        if (nxt.empty()) break;
     }
 
     return dist;
--- graph/shortest_path_faster_algorithm.unit.test.cpp
+++ graph/shortest_path_faster_algorithm.unit.test.cpp
@@ -10,18 +10,31 @@ using namespace std;
 
 int main() {
     default_random_engine gen;
-    REP (iteration, 1000) {
-        int n = iteration + 1;
-        int m = uniform_int_distribution<int>(0, 10 * n)(gen);
+    auto generate_digraph = [&](int n, int m) {
         vector<vector<pair<int, int64_t> > > g(n);
         REP (e, m) {
             int x = uniform_int_distribution<int>(0, n - 1)(gen);
             int y = uniform_int_distribution<int>(0, n - 1)(gen);
-            int64_t cost = uniform_int_distribution<int>(- 1000, 10000)(gen);
+            int64_t cost = uniform_int_distribution<int64_t>(- 1000000, 10000000)(gen);
             g[x].emplace_back(y, cost);
         }
+        return g;
+    };
+
+    // compare with Bellman Ford
+    REP (iteration, 200) {
+        int n = iteration + 1;
+        int m = uniform_int_distribution<int>(0, 10 * n)(gen);
+        auto g = generate_digraph(n, m);
         int root = uniform_int_distribution<int>(0, n - 1)(gen);
         assert (bellman_ford_shortest_path(root, g) == shortest_path_faster_algorithm(root, g));
     }
+
+    // run against big graph
+    int n = 10000;
+    int m = 100000;
+    auto g = generate_digraph(n, m);
+    int root = uniform_int_distribution<int>(0, n - 1)(gen);
+    volatile auto dist = shortest_path_faster_algorithm(root, g);
     return 0;
 }
