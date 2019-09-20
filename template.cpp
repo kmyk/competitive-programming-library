a786a693be506bda373dfa6d50ffa47dfa8043c8
Kimiyuki Onaka <kimiyuki95@gmail.com>
2019-09-21 00:15:25 +0900

    update template.cpp

--- template.cpp
+++ template.cpp
@@ -6,15 +6,14 @@ cat <<EOF
 #define REP3(i, m, n) for (int i = (m); (i) < (int)(n); ++ (i))
 #define REP_R(i, n) for (int i = (int)(n) - 1; (i) >= 0; -- (i))
 #define REP3R(i, m, n) for (int i = (int)(n) - 1; (i) >= (int)(m); -- (i))
-#define ALL(x) begin(x), end(x)
+#define ALL(x) std::begin(x), std::end(x)
 #define dump(x) cerr << #x " = " << x << endl
-using ll = long long;
 using namespace std;
 template <class T> using reversed_priority_queue = priority_queue<T, vector<T>, greater<T> >;
 template <class T, class U> inline void chmax(T & a, U const & b) { a = max<T>(a, b); }
 template <class T, class U> inline void chmin(T & a, U const & b) { a = min<T>(a, b); }
-template <typename X, typename T> auto make_vectors(X x, T a) { return vector<T>(x, a); }
-template <typename X, typename Y, typename Z, typename... Zs> auto make_vectors(X x, Y y, Z z, Zs... zs) { auto cont = make_vectors(y, z, zs...); return vector<decltype(cont)>(x, cont); }
+template <typename X, typename T> auto make_table(X x, T a) { return vector<T>(x, a); }
+template <typename X, typename Y, typename Z, typename... Zs> auto make_table(X x, Y y, Z z, Zs... zs) { auto cont = make_table(y, z, zs...); return vector<decltype(cont)>(x, cont); }
 template <typename T> ostream & operator << (ostream & out, vector<T> const & xs) { REP (i, (int)xs.size() - 1) out << xs[i] << ' '; if (not xs.empty()) out << xs.back(); return out; }
 
 int main() {
