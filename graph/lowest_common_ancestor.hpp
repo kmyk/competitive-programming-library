a2cb6ca5376bfa99900fadc52b973b76dd38250a
Kimiyuki Onaka <kimiyuki95@gmail.com>
2019-11-24 17:26:39 +0900

    update graph/lowest_common_ancestor.hpp

--- graph/lowest_common_ancestor.hpp
+++ graph/lowest_common_ancestor.hpp
@@ -18,6 +18,7 @@ struct lowest_common_ancestor {
     /**
      * @note O(N)
      * @param g is an adjacent list of a tree
+     * @note you can easily modify this to accept forests
      */
     lowest_common_ancestor(int root, std::vector<std::vector<int> > const & g) {
         std::vector<std::pair<int, int> > tour;
@@ -29,12 +30,12 @@ private:
     /**
      * @note to avoid stack overflow
      */
-    void dfs(int i, int parent, int depth, std::vector<std::vector<int> > const & g, std::vector<std::pair<int, int> > & tour) {
-        index[i] = tour.size();
-        tour.emplace_back(depth, i);
-        for (int j : g[i]) if (j != parent) {
-            dfs(j, i, depth + 1, g, tour);
-            tour.emplace_back(depth, i);
+    void dfs(int x, int parent, int depth, std::vector<std::vector<int> > const & g, std::vector<std::pair<int, int> > & tour) {
+        index[x] = tour.size();
+        tour.emplace_back(depth, x);
+        for (int y : g[x]) if (y != parent) {
+            dfs(y, x, depth + 1, g, tour);
+            tour.emplace_back(depth, x);
         }
     }
 public:
@@ -48,5 +49,15 @@ public:
         y = index[y];
         if (x > y) std::swap(x, y);
         return table.range_concat(x, y + 1).second;
+    }
+    int get_depth(int x) const {
+        assert (0 <= x and x < index.size());
+        return table.range_concat(index[x], index[x] + 1).first;
+    }
+    int get_dist(int x, int y) const {
+        assert (0 <= x and x < index.size());
+        assert (0 <= y and y < index.size());
+        int z = (*this)(x, y);
+        return get_depth(x) + get_depth(y) - 2 * get_depth(z);
     }
 };
