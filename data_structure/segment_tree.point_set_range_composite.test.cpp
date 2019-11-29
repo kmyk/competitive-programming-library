cfd6328bf6b93e9ae9917277d290b0743916fc4f
Kimiyuki Onaka <kimiyuki95@gmail.com>
2019-11-29 09:41:35 +0900

    add data_structure/segment_tree.point_set_range_composite.test.cpp

---
+++ data_structure/segment_tree.point_set_range_composite.test.cpp
@@ -0,0 +1,28 @@
+#define PROBLEM "https://judge.yosupo.jp/problem/point_set_range_composite"
+#include "data_structure/segment_tree.hpp"
+#include "utils/monoids.hpp"
+#include "modulus/mint.hpp"
+#include "utils/macros.hpp"
+#include <cstdint>
+#include <tuple>
+using namespace std;
+
+constexpr int MOD = 998244353;
+int main() {
+    int n, q; scanf("%d%d", &n, &q);
+    segment_tree<linear_function_monoid<mint<MOD> > > segtree(n);
+    REP (i, n) {
+        int a, b; scanf("%d%d", &a, &b);
+        segtree.point_set(n - i - 1, make_pair(a, b));
+    }
+    while (q --) {
+        int f, x, y, z; scanf("%d%d%d%d", &f, &x, &y, &z);
+        if (f == 0) {
+            segtree.point_set(n - x - 1, make_pair(y, z));
+        } else if (f == 1) {
+            mint<MOD> a, b; tie(a, b) = segtree.range_concat(n - y, n - x);
+            printf("%d\n", (a * z + b).value);
+        }
+    }
+    return 0;
+}
