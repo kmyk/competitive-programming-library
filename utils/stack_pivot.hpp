26b9e911d979a8ec678ad3544ead40bbd3ff4878
Kimiyuki Onaka <kimiyuki95@gmail.com>
2019-12-20 11:08:16 +0900

    update utils/stack_pivot.hpp

--- utils/stack_pivot.hpp
+++ utils/stack_pivot.hpp
@@ -2,10 +2,9 @@
 #include <cstdlib>
 
 /**
- * @note use as int main() { STACK_PIVOT main_(); exit(EXIT_SUCCESS); }
+ * @note use as int main() { STACK_PIVOT(1 << 30) main_(); exit(EXIT_SUCCESS); }
  */
-#define STACK_PIVOT \
-    size_t stack_size = 1 << 26; \
-    char *malloced = (char *)malloc(stack_size); \
-    char *addr = malloced + stack_size - sizeof(int); \
-    __asm__("mov %0, %%rsp;" : "=g" (addr) );
+#define STACK_PIVOT(STACK_SIZE) \
+    char *malloced = (char *)malloc(STACK_SIZE); \
+    char *addr = malloced + (STACK_SIZE) - sizeof(int); \
+    __asm__("mov %0, %%rsp; call" : "=g" (addr) );
