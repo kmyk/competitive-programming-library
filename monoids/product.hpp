9f4e10350c899cc574d99e50d532cf320ddb9c6c
Kimiyuki Onaka <kimiyuki95@gmail.com>
2019-12-27 19:38:00 +0900

    Add monoids/product.hpp

---
+++ monoids/product.hpp
@@ -0,0 +1,15 @@
+#pragma once
+#include <utility>
+
+template <class Monoid1, class Monoid2>
+struct productt_monoid {
+    typedef std::pair<typename Monoid1::value_type, typename Monoid2::value_type> value_type;
+    Monoid1 mon1;
+    Monoid2 mon2;
+    value_type unit() const {
+        return std::make_pair(mon1.unit(), mon2.unit());
+    }
+    value_type mult(value_type a, value_type b) const {
+        return std::make_pair(mon1.mult(a.first, b.first), mon2.mult(a.second, b.second));
+    }
+};
