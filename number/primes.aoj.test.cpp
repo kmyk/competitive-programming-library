3b605d7c82399c2e607b5393204fcbe604a837ad
Kimiyuki Onaka <kimiyuki95@gmail.com>
2019-12-29 19:02:34 +0900

    Use scanf in number/primes.aoj.test.cpp

--- number/primes.aoj.test.cpp
+++ number/primes.aoj.test.cpp
@@ -1,16 +1,16 @@
 #define PROBLEM "http://judge.u-aizu.ac.jp/onlinejudge/description.jsp?id=ALDS1_1_C"
 #include "number/primes.hpp"
 #include <cassert>
-#include <iostream>
+#include <cstdio>
 
 int main() {
-    int n; std::cin >> n;
+    int n; scanf("%d", &n);
     prepared_primes primes(1e6 + 3);
     int cnt = 0;
     REP (i, n) {
-        int a; std::cin >> a;
+        int a; scanf("%d", &a);
         cnt += primes.is_prime(a);
     }
-    std::cout << cnt << std::endl;
+    printf("%d\n", cnt);
     return 0;
 }
