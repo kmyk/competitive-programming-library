8f1bb1802b34e2db59e850d889d3a1ef0dc38a04
Kimiyuki Onaka <kimiyuki95@gmail.com>
2019-12-29 19:05:48 +0900

    Use scanf instad of cin in data_structure/lazy_propagation_segment_tree.range_min_range_add.test.cpp

--- data_structure/lazy_propagation_segment_tree.range_min_range_add.test.cpp
+++ data_structure/lazy_propagation_segment_tree.range_min_range_add.test.cpp
@@ -3,22 +3,21 @@
 #include "monoids/min.hpp"
 #include "monoids/plus.hpp"
 #include "monoids/plus_min_action.hpp"
-
-#include <iostream>
+#include <cstdio>
 using namespace std;
 
 int main() {
-    int n, q; cin >> n >> q;
+    int n, q; scanf("%d%d", &n, &q);
     lazy_propagation_segment_tree<min_monoid<int>, plus_monoid<int>, plus_min_action<int> > segtree(n);
     segtree.range_set(0, n, 0);
     while (q --) {
-        int com, l, r; cin >> com >> l >> r;
+        int com, l, r; scanf("%d%d%d", &com, &l, &r);
         ++ r;
         if (com == 0) {
-            int x; cin >> x;
+            int x; scanf("%d", &x);
             segtree.range_apply(l, r, x);
         } else if (com == 1) {
-            cout << segtree.range_concat(l, r) << endl;
+            printf("%d\n", segtree.range_concat(l, r));
         }
     }
     return 0;
