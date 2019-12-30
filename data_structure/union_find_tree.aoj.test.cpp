c34d381846a77509c281b62410ffe7d03164f064
Kimiyuki Onaka <kimiyuki95@gmail.com>
2019-12-30 22:19:09 +0900

    Add data_structure/union_find_tree.yosupo.test.cpp

--- data_structure/union_find_tree.aoj.test.cpp
+++ data_structure/union_find_tree.aoj.test.cpp
@@ -1,18 +1,17 @@
 #define PROBLEM "http://judge.u-aizu.ac.jp/onlinejudge/description.jsp?id=DSL_1_A"
 #include "data_structure/union_find_tree.hpp"
-
-#include <iostream>
-using namespace std;
+#include <cstdio>
 
 int main() {
-    int n, q; cin >> n >> q;
+    int n, q; scanf("%d%d", &n, &q);
     union_find_tree uft(n);
     while (q --) {
-        int com, x, y; cin >> com >> x >> y;
+        int com, x, y; scanf("%d%d%d", &com, &x, &y);
         if (com == 0) {
             uft.unite_trees(x, y);
         } else if (com == 1) {
-            cout << uft.is_same(x, y) << endl;
+            bool answer = uft.is_same(x, y);
+            printf("%d\n", (int)answer);
         }
     }
     return 0;
---
+++ data_structure/union_find_tree.yosupo.test.cpp
@@ -0,0 +1,18 @@
+#define PROBLEM "https://judge.yosupo.jp/problem/unionfind"
+#include "data_structure/union_find_tree.hpp"
+#include <cstdio>
+
+int main() {
+    int n, q; scanf("%d%d", &n, &q);
+    union_find_tree uft(n);
+    while (q --) {
+        int t, u, v; scanf("%d%d%d", &t, &u, &v);
+        if (t == 0) {
+            uft.unite_trees(u, v);
+        } else if (t == 1) {
+            bool answer = uft.is_same(u, v);
+            printf("%d\n", (int)answer);
+        }
+    }
+    return 0;
+}
