c59f0ea73501bc609c0c5cf9187d340daaa936b6
Kimiyuki Onaka <kimiyuki95@gmail.com>
2020-01-08 18:24:20 +0900

    Add new fastio

--- data_structure/segment_tree_beats.yosupo.test.cpp
+++ data_structure/segment_tree_beats.yosupo.test.cpp
@@ -4,39 +4,35 @@
 #include <cstdint>
 #include <vector>
 #include "utils/macros.hpp"
-#include "utils/fastio_scanner.hpp"
-#include "utils/fastio_printer.hpp"
+#include "utils/fastio.hpp"
 
 int main() {
-    scanner sc;
-    printer pr;
-
-    int n = sc.get<int>();
-    int q = sc.get<int>();
+    int n = in<int>();
+    int q = in<int>();
 
     std::vector<int64_t> a(n);
     REP (i, n) {
-        a[i] = sc.get<int64_t>();
+        a[i] = in<int64_t>();
     }
     segment_tree_beats beats(ALL(a));
 
     while (q --) {
-        int ty = sc.get<int>();
-        int l = sc.get<int>();
-        int r = sc.get<int>();
+        int ty = in<int>();
+        int l = in<int>();
+        int r = in<int>();
         if (ty == 0) {
-            int64_t b = sc.get<int64_t>();
+            int64_t b = in<int64_t>();
             beats.range_chmin(l, r, b);
         } else if (ty == 1) {
-            int64_t b = sc.get<int64_t>();
+            int64_t b = in<int64_t>();
             beats.range_chmax(l, r, b);
         } else if (ty == 2) {
-            int64_t b = sc.get<int64_t>();
+            int64_t b = in<int64_t>();
             beats.range_add(l, r, b);
         } else {
             int64_t sum = beats.range_sum(l, r);
-            pr.put<int64_t>(sum);
-            pr.put<char>('\n');
+            out<int64_t>(sum);
+            out<char>('\n');
         }
     }
     return 0;
---
+++ utils/fastio.hpp
@@ -0,0 +1,34 @@
+#pragma once
+#include <cstdint>
+#include <cstdio>
+#include <string>
+#include <type_traits>
+
+template <class Char, std::enable_if_t<std::is_same_v<Char, char>, int> = 0>
+inline Char in() { return getchar_unlocked(); }
+template <class String, std::enable_if_t<std::is_same_v<String, std::string>, int> = 0>
+inline std::string in() {
+    std::string s;
+    for (char c; not isspace(c = getchar_unlocked()); ) s.push_back(c);
+    return s;
+}
+template <class Integer, std::enable_if_t<std::is_integral_v<Integer>, int> = 0>
+inline Integer in() {
+    Integer n = getchar_unlocked() - '0';
+    if (std::is_signed<Integer>::value and n + '0' == '-') return -in<Integer>();
+    for (char c; (c = getchar_unlocked()) >= '0'; ) n = n * 10 + c - '0';
+    return n;
+}
+
+template <class Char, std::enable_if_t<std::is_same_v<Char, char>, int> = 0>
+inline void out(char c) { putchar_unlocked(c); }
+template <class String, std::enable_if_t<std::is_same_v<String, std::string>, int> = 0>
+inline void out(const std::string & s) { for (char c : s) putchar_unlocked(c); }
+template <class Integer, std::enable_if_t<std::is_integral_v<Integer>, int> = 0>
+inline void out(Integer n) {
+    char s[20];
+    int i = 0;
+    if (std::is_signed<Integer>::value and n < 0) { putchar_unlocked('-'); n *= -1; }
+    do { s[i ++] = n % 10; n /= 10; } while (n);
+    while (i) putchar_unlocked(s[-- i] + '0');
+}
