61dd0db78c6d743b0b17c1b59c45caaa5ffff32e
Kimiyuki Onaka <kimiyuki95@gmail.com>
2020-01-08 18:45:57 +0900

    Update fastio unlocked

--- hack/fastio_unlocked.hpp
+++ hack/fastio_unlocked.hpp
@@ -8,3 +8,11 @@ inline int64_t getint_unlocked() {
     for (char c; (c = getchar_unlocked()) >= '0'; ) n = n * 10 + c - '0';
     return n;
 }
+
+inline void putint_unlocked(int64_t n) {
+    char s[20];
+    int i = 0;
+    if (n < 0) { putchar_unlocked('-'); n *= -1; }
+    do { s[i ++] = n % 10; n /= 10; } while (n);
+    while (i) putchar_unlocked(s[-- i] + '0');
+}
