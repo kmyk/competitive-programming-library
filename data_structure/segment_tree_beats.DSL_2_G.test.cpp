946f9b367e33e8e9f5c0feaeb6f1d90897edafe1
Kimiyuki Onaka <kimiyuki95@gmail.com>
2020-01-23 16:30:06 +0900

    Add data_structure/segment_tree_beats.DSL_2_{G,H,I}.test.cpp

---
+++ data_structure/segment_tree_beats.DSL_2_G.test.cpp
@@ -0,0 +1,30 @@
+#define PROBLEM "http://judge.u-aizu.ac.jp/onlinejudge/description.jsp?id=DSL_2_G"
+#include "data_structure/segment_tree_beats.hpp"
+
+#include <cstdint>
+#include <vector>
+#include "utils/macros.hpp"
+#include "hack/fastio.hpp"
+
+int main() {
+    int n = in<int>();
+    int q = in<int>();
+
+    segment_tree_beats beats(n);
+
+    while (q --) {
+        int ty = in<int>();
+        int l = in<int>();
+        int r = in<int>();
+        -- l;
+        if (ty == 0) {
+            int64_t x = in<int64_t>();
+            beats.range_add(l, r, x);
+        } else {
+            int64_t sum = beats.range_sum(l, r);
+            out<int64_t>(sum);
+            out<char>('\n');
+        }
+    }
+    return 0;
+}
---
+++ data_structure/segment_tree_beats.DSL_2_H.test.cpp
@@ -0,0 +1,30 @@
+#define PROBLEM "http://judge.u-aizu.ac.jp/onlinejudge/description.jsp?id=DSL_2_H"
+#include "data_structure/segment_tree_beats.hpp"
+
+#include <cstdint>
+#include <vector>
+#include "utils/macros.hpp"
+#include "hack/fastio.hpp"
+
+int main() {
+    int n = in<int>();
+    int q = in<int>();
+
+    segment_tree_beats beats(n);
+
+    while (q --) {
+        int ty = in<int>();
+        int l = in<int>();
+        int r = in<int>();
+        ++ r;
+        if (ty == 0) {
+            int64_t x = in<int64_t>();
+            beats.range_add(l, r, x);
+        } else {
+            int64_t min = beats.range_min(l, r);
+            out<int64_t>(min);
+            out<char>('\n');
+        }
+    }
+    return 0;
+}
