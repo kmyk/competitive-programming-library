3cce5b03935b1cda7f692d6567b3db7f1e48d1eb
Kimiyuki Onaka <kimiyuki95@gmail.com>
2020-03-04 16:22:43 +0900

    Add hack/fastio.yosupo.test.cpp

---
+++ hack/fastio.yosupo.test.cpp
@@ -0,0 +1,13 @@
+#define PROBLEM "https://judge.yosupo.jp/problem/many_aplusb"
+#include "hack/fastio.hpp"
+
+int main() {
+    int t = in<int>();
+    while (t --) {
+        uint64_t a = in<uint64_t>();
+        uint64_t b = in<uint64_t>();
+        out<uint64_t>(a + b);
+        out<char>('\n');
+    }
+    return 0;
+}
