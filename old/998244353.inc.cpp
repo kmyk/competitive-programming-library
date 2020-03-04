b2c7882bf66b0ddd4624178f6638998fcbdfbb19
Kimiyuki Onaka <kimiyuki95@gmail.com>
2020-03-04 13:18:45 +0900

    Delete some files in old/

--- old/998244353.inc.cpp
+++
@@ -1,1 +0,0 @@
-constexpr int PRIME = 998244353;  // 2^23 * 7 * 17 + 1
--- old/choose.inc.cpp
+++
@@ -1,8 +0,0 @@
-double choose(int n, int r) {
-    double acc = 1;
-    REP (i, r) {
-        acc *= n - i;
-        acc /= i + 1;
-    }
-    return acc;
-}
