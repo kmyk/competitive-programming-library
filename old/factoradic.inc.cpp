60727bdd7ff0a8284d61b7c53a7735a2f5808996
Kimiyuki Onaka <kimiyuki95@gmail.com>
2020-05-23 00:47:48 +0900

    Remove old/factoradic.inc.cpp

--- old/factoradic.inc.cpp
+++ old/factoradic.inc.cpp
@@ -1,23 +1,30 @@
-int fact(int n) { return n ? n * fact(n-1) : 1; } // O(N)
 template <class RandomAccessIterator>
 uint64_t encode_factoradic(RandomAccessIterator first, RandomAccessIterator last) { // O(N^2)
+    static vector<int> fact(1, 1);
+    while (fact.size() < n) {
+        fact.push_back(fact.size() * fact.back());
+    }
     int n = last - first;
     uint64_t y = 0;
-    repeat (i,n) {
+    REP (i, n) {
         int xi = *(first + i);
         int rank = count_if(first, first + i, [&](int xj) { return xi < xj; });
-        y += rank * fact(i);
+        y += rank * fact[i];
     }
     return y;
 }
 vector<int> decode_factoradic(uint64_t y, int n) { // O(N^2)
+    static vector<int> fact(1, 1);
+    while (fact.size() < n) {
+        fact.push_back(fact.size() * fact.back());
+    }
     vector<int> xs(n);
     vector<int> zs(n); iota(zs.begin(), zs.end(), 0);
-    repeat_reverse (i,n) {
-        auto it = zs.begin() + (i - y / fact(i));
+    REP_R (i, n) {
+        auto it = zs.begin() + (i - y / fact[i]);
         xs[i] = *it;
         zs.erase(it);
-        y %= fact(i);
+        y %= fact[i];
     }
     return xs;
 }
