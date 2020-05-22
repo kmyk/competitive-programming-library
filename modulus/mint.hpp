3976e8472c0fb19c5340fa277b3d2433585830ae
Kimiyuki Onaka <kimiyuki95@gmail.com>
2020-05-23 00:48:03 +0900

    Remove constexpr from mint.hpp for Topcoder

--- modulus/mint.hpp
+++ modulus/mint.hpp
@@ -16,19 +16,19 @@ struct mint {
     mint(int64_t value_) : value(value_ < 0 ? value_ % MOD + MOD : value_ >= MOD ? value_ % MOD : value_) {}
     mint(int32_t value_, std::nullptr_t) : value(value_) {}
     explicit operator bool() const { return value; }
-    inline constexpr mint<MOD> operator + (mint<MOD> other) const { return mint<MOD>(*this) += other; }
-    inline constexpr mint<MOD> operator - (mint<MOD> other) const { return mint<MOD>(*this) -= other; }
-    inline constexpr mint<MOD> operator * (mint<MOD> other) const { return mint<MOD>(*this) *= other; }
-    inline constexpr mint<MOD> & operator += (mint<MOD> other) { this->value += other.value; if (this->value >= MOD) this->value -= MOD; return *this; }
-    inline constexpr mint<MOD> & operator -= (mint<MOD> other) { this->value -= other.value; if (this->value <    0) this->value += MOD; return *this; }
-    inline constexpr mint<MOD> & operator *= (mint<MOD> other) { this->value = (uint_fast64_t)this->value * other.value % MOD; return *this; }
-    inline constexpr mint<MOD> operator - () const { return mint<MOD>(this->value ? MOD - this->value : 0, nullptr); }
-    inline constexpr mint<MOD> pow(uint64_t k) const { return mint<MOD>(modpow(value, k, MOD), nullptr); }
+    inline mint<MOD> operator + (mint<MOD> other) const { return mint<MOD>(*this) += other; }
+    inline mint<MOD> operator - (mint<MOD> other) const { return mint<MOD>(*this) -= other; }
+    inline mint<MOD> operator * (mint<MOD> other) const { return mint<MOD>(*this) *= other; }
+    inline mint<MOD> & operator += (mint<MOD> other) { this->value += other.value; if (this->value >= MOD) this->value -= MOD; return *this; }
+    inline mint<MOD> & operator -= (mint<MOD> other) { this->value -= other.value; if (this->value <    0) this->value += MOD; return *this; }
+    inline mint<MOD> & operator *= (mint<MOD> other) { this->value = (uint_fast64_t)this->value * other.value % MOD; return *this; }
+    inline mint<MOD> operator - () const { return mint<MOD>(this->value ? MOD - this->value : 0, nullptr); }
+    inline mint<MOD> pow(uint64_t k) const { return mint<MOD>(modpow(value, k, MOD), nullptr); }
     inline mint<MOD> inv() const { return mint<MOD>(modinv(value, MOD), nullptr); }
-    inline constexpr mint<MOD> operator /  (mint<MOD> other) const { return *this *  other.inv(); }
-    inline constexpr mint<MOD> operator /= (mint<MOD> other)       { return *this *= other.inv(); }
-    inline constexpr bool operator == (mint<MOD> other) const { return value == other.value; }
-    inline constexpr bool operator != (mint<MOD> other) const { return value != other.value; }
+    inline mint<MOD> operator /  (mint<MOD> other) const { return *this *  other.inv(); }
+    inline mint<MOD> operator /= (mint<MOD> other)       { return *this *= other.inv(); }
+    inline bool operator == (mint<MOD> other) const { return value == other.value; }
+    inline bool operator != (mint<MOD> other) const { return value != other.value; }
 };
 template <int32_t MOD> mint<MOD> operator * (int64_t value, mint<MOD> n) { return mint<MOD>(value) * n; }
 template <int32_t MOD> std::istream & operator >> (std::istream & in, mint<MOD> & n) { int64_t value; in >> value; n = value; return in; }
--- modulus/modpow.hpp
+++ modulus/modpow.hpp
@@ -1,13 +1,13 @@
 #pragma once
 #include <cassert>
 
-inline constexpr int32_t modpow(uint_fast64_t x, uint64_t k, int32_t MOD) {
-    assert (0 <= x and x < MOD);
+inline int32_t modpow(uint_fast64_t x, uint64_t k, int32_t MOD) {
+    assert (/* 0 <= x and */ x < (uint_fast64_t)MOD);
     uint_fast64_t y = 1;
     for (; k; k >>= 1) {
         if (k & 1) (y *= x) %= MOD;
         (x *= x) %= MOD;
     }
-    assert (0 <= y and y < MOD);
+    assert (/* 0 <= y and */ y < (uint_fast64_t)MOD);
     return y;
 }
