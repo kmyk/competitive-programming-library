2adb887aff5f1b4f63e21ff4a410814cada2238b
Kimiyuki Onaka <kimiyuki95@gmail.com>
2020-05-23 03:04:39 +0900

    Add utils/binary_search_float.hpp

---
+++ utils/binary_search_float.hpp
@@ -0,0 +1,16 @@
+#pragma once
+#include <cassert>
+#include "utils/macros.hpp"
+
+/**
+ * @brief a binary search on floating point numbers / 二分探索 (浮動小数点数)
+ */
+template <int Iteration = 64, typename UnaryPredicate>
+double binsearch_float(double l, double r, UnaryPredicate p) {
+    assert (l <= r);
+    REP (i, Iteration) {
+        double m = (l + r) / 2;
+        (p(m) ? r : l) = m;
+    }
+    return r;
+}
