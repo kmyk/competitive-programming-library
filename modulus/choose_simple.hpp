c23296c5b9f2f24a0036bf6ca9b8e626fdf6423c
Kimiyuki Onaka <kimiyuki95@gmail.com>
2020-05-23 00:48:26 +0900

    Add modulus/multichoose_simple.hpp

--- modulus/choose_simple.hpp
+++ modulus/choose_simple.hpp
@@ -7,7 +7,7 @@
  * @brief combination / 組合せ ${} _ n C _ r$ (愚直 $O(r)$)
  */
 template <int32_t MOD>
-mint<MOD> choose_simple(int n, int r) {
+mint<MOD> choose_simple(int64_t n, int32_t r) {
     assert (0 <= r and r <= n);
     mint<MOD> num = 1;
     mint<MOD> den = 1;
---
+++ modulus/multichoose_simple.hpp
@@ -0,0 +1,14 @@
+#pragma once
+#include <cassert>
+#include "modulus/mint.hpp"
+#include "modulus/choose_simple.hpp"
+
+/**
+ * @brief 重複組合せ ${} _ n H _ r = {} _ {n + r - 1} C _ r$ (愚直 $O(r)$)
+ */
+template <int32_t MOD>
+mint<MOD> multichoose_simple(int64_t n, int32_t r) {
+    assert (0 <= n and 0 <= r);
+    if (n == 0 and r == 0) return 1;
+    return choose_simple<MOD>(n + r - 1, r);
+}
