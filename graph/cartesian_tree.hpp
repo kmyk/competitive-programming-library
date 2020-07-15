f51247221743d4d0edd3c026eb8422461496caaf
Kimiyuki Onaka <kimiyuki95@gmail.com>
2020-07-16 00:31:29 +0900

    Fix Cartesian tree

--- graph/cartesian_tree.hpp
+++ graph/cartesian_tree.hpp
@@ -14,14 +14,15 @@ std::vector<int> construct_cartesian_tree(const std::vector<T> & a, const Compar
     int n = a.size();
     std::vector<int> parent(n, -1);
     REP3 (i, 1, n) {
-        int p = i - 1;
-        int l = -1;
+        int p = i - 1;  // parent of i
+        int l = -1;  // left child of i
         while (p != -1 and cmp(a[i], a[p])) {
-            int pp = parent[p];
+            int pp = parent[p];  // parent of parent of i
             if (l != -1) {
                 parent[l] = p;
             }
             parent[p] = i;
+            l = p;
             p = pp;
         }
         parent[i] = p;
