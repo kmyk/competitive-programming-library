8e5704a243ab4a9efe34f05cd6ae8c1fa225cc69
Kimiyuki Onaka <kimiyuki95@gmail.com>
2021-04-23 11:00:27 +0900

    Add modulus/mint_with_zero.test.cpp

--- modulus/mint_with_zero.hpp
+++ modulus/mint_with_zero.hpp
@@ -3,16 +3,18 @@
 #include <iostream>
 #include "modulus/modpow.hpp"
 #include "modulus/modinv.hpp"
+#include "modulus/mint.hpp"
 
 /**
- * @brief 零の個数付き剰余演算の乗法群 $\mathbb{Z}/n\mathbb{Z} \times \mathbb{Z}$
+ * @brief $\mathbb{Z}$ の乗除算を $\mathbb{Z}/n\mathbb{Z}$ の上でやるデータ構造
+ * @sa https://kimiyuki.net/blog/2021/04/23/modulo-with-zero/
  */
 template <int32_t MOD>
 struct zmint {
     int32_t nonzero;
     int32_t zero;
-    zmint() : nonzero() {}
-    zmint(int64_t nonzero_, int32_t zero_ = 0) : nonzero(nonzero_), zero(zero_) { while (nonzero and nonzero % MOD == 0) { nonzero_ /= MOD; zero_ += 1; } if (nonzero_ < 0) nonzero_ = nonzero_ % MOD + MOD; }
+    zmint() : nonzero(1) {}
+    zmint(int64_t nonzero_, int32_t zero_ = 0) : nonzero(nonzero_), zero(zero_) { assert (nonzero != 0); while (nonzero % MOD == 0) { nonzero /= MOD; zero += 1; } if (nonzero < 0) nonzero = nonzero % MOD + MOD; }
     zmint(int32_t nonzero_, int32_t zero_, std::nullptr_t) : nonzero(nonzero_), zero(zero_) {}
     explicit operator bool() const { return nonzero; }
     inline zmint<MOD> operator * (zmint<MOD> other) const { return zmint<MOD>(*this) *= other; }
@@ -24,6 +26,7 @@ struct zmint {
     inline bool operator != (zmint<MOD> other) const { return nonzero != other.nonzero or zero != other.zero; }
     inline zmint<MOD> pow(uint64_t k) const { return zmint<MOD>(modpow(nonzero, k, MOD), k * zero, nullptr); }
     inline zmint<MOD> inv() const { return zmint<MOD>(modinv(nonzero, MOD), - zero, nullptr); }
+    inline mint<MOD> to_mint() const { assert (nonzero >= 0); return zero ? 0 : nonzero; }
 };
 template <int32_t MOD> zmint<MOD> operator * (int64_t nonzero, zmint<MOD> n) { return zmint<MOD>(nonzero) * n; }
 template <int32_t MOD> zmint<MOD> operator / (int64_t nonzero, zmint<MOD> n) { return zmint<MOD>(nonzero) / n; }
---
+++ modulus/mint_with_zero.test.cpp
@@ -0,0 +1,46 @@
+#define PROBLEM "https://atcoder.jp/contests/arc117/tasks/arc117_c"
+#include <array>
+#include <cassert>
+#include <iostream>
+#include <vector>
+#include "utils/macros.hpp"
+#include "modulus/mint.hpp"
+#include "modulus/mint_with_zero.hpp"
+using namespace std;
+
+int solve(int n, const std::vector<int>& a) {
+    std::vector<zmint<3> > fact(n + 1);
+    fact[0] = 1;
+    REP (i, n) {
+        fact[i + 1] = (i + 1) * fact[i];
+    }
+    auto choose = [&](int n, int r) {
+        return (fact[n] * fact[n - r].inv() * fact[r].inv()).to_mint();
+    };
+
+    mint<3> b = 0;
+    REP (i, n) {
+        b += choose(n - 1, i) * a[i];
+    }
+    if (n % 2 == 0) {
+        b *= -1;
+    }
+    return b.value;
+}
+
+int main() {
+    int n; std::cin >> n;
+    std::string s; std::cin >> s;
+    assert (s.length() == n);
+    std::vector<int> a(n);
+    REP (i, n) {
+        a[i] = (s[i] == 'W' ? 0 : s[i] == 'R' ? 1 : s[i] == 'B' ? 2 : -1);
+        assert (a[i] != -1);
+    }
+    int ans = solve(n, a);
+    assert (0 <= ans and ans < 3);
+    std::array<char, 3> table = {{ 'W', 'R', 'B' }};
+    std::cout << table[ans] << std::endl;
+    return 0;
+}
+
