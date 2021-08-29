869c39480e13aa547ab02a94f7cf50c20fb337cb
Kimiyuki Onaka <kimiyuki95@gmail.com>
2021-08-30 02:09:39 +0900

    Add number/inversion_number.hpp

---
+++ number/inversion_number.hpp
@@ -0,0 +1,17 @@
+#pragma once
+#include <cstdint>
+#include <vector>
+#include "data_structure/binary_indexed_tree.hpp"
+#include "utils/macros.hpp"
+#include "monoids/plus.hpp"
+
+int64_t inversion_number(const std::vector<int> &a) {
+    int n = a.size();
+    binary_indexed_tree<plus_monoid<int64_t>> bit(n + 1);
+    int64_t cnt = 0;
+    REP (i, n) {
+        cnt += i - bit.initial_range_get(a[i] + 1);
+        bit.point_mult(a[i], 1);
+    }
+    return cnt;
+}
